4ef51b2a6e16c4cf6a8cbc9af4d3a2ed845b7507
haiqal <muhammadhaiqaldani@gmail.com>
2022-12-30 15:12:23 +0700

    change link

--- services/supplier/getDetailSupplier.ts
+++ services/supplier/getDetailSupplier.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const ROOT_API = "http://localhost:3001";
+const ROOT_API = "https://t3vraj0nbd.execute-api.us-east-1.amazonaws.com/";
 
 export async function getDetailSupplier(id: string) {
   const URL = `supplier/get-supplier/${id}`;
